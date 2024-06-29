10e0a1b203c340c8224e767898ab1c918e669445
Astral890 <enac7125@outlook.com>
2024-06-28 19:17:58 -0600

    Detalles finales

--- utils.js
+++ utils.js
@@ -1,5 +1,3 @@
-const li=document.getElementById('completadas');
-li.addEventListener('click',eliminar());
 function incluirtarea(){
     const toDo= document.getElementById('pendientes');
     let text=document.getElementById('tana').value;
@@ -7,32 +5,33 @@ function incluirtarea(){
         // Tarea individual
         const li=document.createElement('li');
         li.id="listas";
+        li.className="list-group-item"
         //Un div
         const divint=document.createElement('div')
         divint.id="divlis"
         // Creacion parrafo
         const snap=document.createElement('snap');
         snap.innerHTML=text;
+        snap.className="snapFont"
         // Creacion boton
         const check=document.createElement('button')
         check.id='complete';
-        check.innerHTML='Completada';
+        check.className="btn btn-outline-dark";
+        check.innerHTML='Completar';
         check.onclick=completar;
-        // check.onclick=completar();
         //Agrupamiento
         divint.appendChild(snap);
         divint.appendChild(check);
-        li.appendChild(divint)
-        console.log(li);
+        li.appendChild(divint);
         toDo.appendChild(li);
     }
 }
 function completar(event){
     const lic=event.target.parentElement.parentElement;
-    console.log(lic);
     const completadas=document.getElementById('completadas');
     completadas.appendChild(lic);
 }
-function eliminar(){
-    li.remove();
+function deleteTask(){
+    const del=document.getElementById('completadas');
+    del.innerHTML='';
 }
